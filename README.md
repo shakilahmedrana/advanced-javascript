3f58d311b7378cfb8cf6de0e503304b8436e4392
Rana ahmed <shakilahmedrana5@gmail.com>
2020-11-21 23:30:36 +0600

    true vs false

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # advanced-javascript
+# advanced-javascript
---
+++ truthy-vs-false.js
@@ -0,0 +1,9 @@
+// const name = 'Rana';
+let name = 0;
+
+if(name || name == 0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false')
+}
